1c5cd955a4173067452f27b750832f41aaa3eb04
Dipanshu Zalke <dipanshuzalke@gmail.com>
2025-02-22 21:45:47 +0530

    frontend added

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import Page from '../src/app/dashboard/page'
+
+function App() {
+
+  return (
+    <div>
+      <Page />
+  </div>
+  )
+}
+
+export default App
