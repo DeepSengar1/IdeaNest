0a6f8855534642aa821eb5497f4a41b2042ec79f
Rishu Kumar <rishukumar9233@gmail.com>
2025-02-23 19:04:00 +0530

    fixed login-server error

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,12 +7,12 @@ router.post("/", async (req, res) => {
   const { clerkId, name, email, role } = req.body;
   try {
     const user = await User.findOneAndUpdate(
-      { clerkId: userData.clerkId },
+      { clerkId: clerkId },
       {
-        name: userData.name,
-        email: userData.email,
-        role: userData.role,
-        avatarUrl: userData.avatarUrl,
+        name: name,
+        email: email,
+        role: role,
+        // avatarUrl: avatarUrl,
       },
       { new: true, upsert: true }
     );
